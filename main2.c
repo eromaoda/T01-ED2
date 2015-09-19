f91fa78ec2b79bfc41b50afbde7fccab7b5cf017
eromaoda <errocha@openmailbox.org>
2015-09-18 21:16:41 -0300

    verificacoes de entrada no main2

--- main2.c
+++ main2.c
@@ -10,7 +10,19 @@
 #include<stdlib.h>
 #include<string.h>
 
+#define MAX 192
+
+typedef struct reg{
+	char chave[8];
+	int rrn;
+} Registro;
+
 void printMenu();
+int verificaData(char *data);
+int verificaDuracao(char *duracao);
+int verificaPlacar(char *placar);
+int verificaVencedor(char *nomeAzul, char *nomeVermelho, char *winner, char *placarAzul, char *placarVermelho);
+void insereArq(FILE *f, char *na, char *nv, char *data, char *dur, char *win, char *pla, char *plv, char *mvp, char *chave);
 
 int main(){
 	int op;
@@ -18,6 +30,7 @@ int main(){
 	char data[10], duracao[5];
 	char nomeWinner[39], apelidoMVP[39];
 	char placarAzul[2], placarVermelho[2];
+	FILE *matches, *iprim, *isec, *iwinner, *imvp;
 	
 	while(1){
 		printMenu();
@@ -36,12 +49,20 @@ int main(){
 				scanf("%[^\n]s", data);
 				getchar();
 				//Verificar data
-				//...
+				while(verificaData(data) != 1){
+					printf("Campo invalido ! Tente novamente: ");
+					scanf("%[^\n]s", data);
+					getchar();
+				}
 				
 				scanf("%[^\n]s", duracao);
 				getchar();
 				//Verificar duracao
-				//...
+				while(verificaDuracao(duracao) != 1){
+					printf("Campo invalido ! Tente novamente: ");
+					scanf("%[^\n]s", duracao);
+					getchar();
+				}
 				
 				scanf("%[^\n]s", nomeWinner);
 				getchar();
@@ -49,15 +70,27 @@ int main(){
 				scanf("%[^\n]s", placarAzul);
 				getchar();
 				//Verificar placar azul
-				//...
+				while(verificaPlacar(placarAzul) != 1){
+					printf("Campo invalido ! Tente novamente: ");
+					scanf("%[^\n]s", placarAzul);
+					getchar();
+				}
 				
 				scanf("%[^\n]s", placarVermelho);
 				getchar();
 				//Verificar placar vermelho
-				//...
+				while(verificaPlacar(placarVermelho) != 1){
+					printf("Campo invalido ! Tente novamente: ");
+					scanf("%[^\n]s", placarVermelho);
+					getchar();
+				}
 				
 				//Verificar validade do vencedor
-				//...
+				while(verificaVencedor(nomeAzul, nomeVermelho, nomeWinner, placarAzul, placarVermelho) != 1){
+					printf("Campo invalido ! Tente novamente: ");
+					scanf("%[^\n]s", nomeWinner);
+					getchar();
+				}
 				
 				scanf("%[^\n]s", apelidoMVP);
 				getchar();
@@ -73,7 +106,7 @@ int main(){
 				chave[7] = data[4];
 				chave[8] = '\0';
 				
-				printf("chave = %s\n", chave);
+				//Inserir os valores no arquivo matches.dat
 				
 				break;
 			//Remover registro
@@ -105,4 +138,77 @@ int main(){
 void printMenu(){
 	printf("1. Inserir nova partida\n2. Remover partida a partir da chave primaria\n3. Modificar duracao\n4. Buscar partidas\n");
 	printf("5. Listar partidas\n6. Liberar espaco\n7. Finalizar\n");
+}
+
+int verificaData(char *data){
+	char dias[2], meses[2], anos[4];
+	int d, m, a;
+	
+	//Separa os dias, o mese e o ano da data
+	dias[0] = data[0];
+	dias[1] = data[1];
+	
+	meses[0] = data[3];
+	meses[1] = data[4];
+	
+	anos[0] = data[6];
+	anos[1] = data[7];
+	anos[2] = data[8];
+	anos[3] = data[9];
+	
+	//Verifica a validade
+	if(strlen(data) == 10){
+		if(data[2] == '/' && data[5] == '/'){
+			//Passando os valores de char para int
+			//e calculando o numero certo do dia, mes e ano
+			//Subtrair '0' de um caractere numerico nos da o valor inteiro que ele representa
+			d = ((dias[0] - '0') * 10) + (dias[1] - '0');
+			m = ((meses[0] - '0') * 10) + (meses[1] - '0');
+			a = ((anos[0] - '0') * 1000) + ((anos[1] - '0') * 100) + ((anos[2] - '0') * 10) + (anos[3] - '0');
+			
+			if((d > 0 && d < 32) && (m > 0 && m < 13) && (a > 2010 && a < 2016)) return 1;
+		}
+	}
+	
+	return 0;
+}
+
+int verificaDuracao(char *duracao){
+	if(strlen(duracao) == 5){
+		if(duracao[2] == ':') return 1;
+	}
+	
+	return 0;
+}
+
+int verificaPlacar(char *placar){
+	if(strlen(placar) == 2) return 1;
+	
+	return 0;
+}
+
+int verificaVencedor(char *nomeAzul, char *nomeVermelho, char *winner, char *placarAzul, char *placarVermelho){
+	int pa, pv;
+	char *aux;
+	
+	aux = malloc(sizeof(char) * MAX + 1);
+	if(aux == NULL) exit(1);
+	
+	pa = ((placarAzul[0] - '0') * 10) + (placarAzul[1] - '0');
+	pv = ((placarVermelho[0] - '0') * 10) + (placarVermelho[1] - '0');
+	
+	if(pa > pv) strcpy(aux, nomeAzul);
+	else if(pv > pa) strcpy(aux, nomeVermelho);
+
+	if(strcmp(aux, winner) == 0){
+		free(aux);
+		return 1;
+	}
+	
+	free(aux);
+	return 0;
+}
+
+void insereArq(FILE *f, char *na, char *nv, char *data, char *dur, char *win, char *pla, char *plv, char *mvp, char *chave){
+	
 }
